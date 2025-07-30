c8285d2c1233f37a98d1cf4dad54d2af0cfd409b
Luis Gubler <your_email@example.com>
2025-07-30 14:05:30 -0400

    fix: Define props for ChatBackground component

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,4 +1,4 @@
-export { default as ChatBackground } from './ChatBackground';
+export { ChatBackground } from './ChatBackground';
 export { MessageList   } from './MessageList';
 export { ChatFooter    } from './ChatFooter';
 
